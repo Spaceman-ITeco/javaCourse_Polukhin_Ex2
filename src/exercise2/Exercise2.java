3d85127366c14887641734eef8305f600aea85d5
@poluhin <poluhin@i-teco.ru>
2021-12-04 16:58:49 +0300

    Practical exercise 2

---
+++ src/exercise2/Exercise2.java
@@ -0,0 +1,45 @@
+package exercise2;
+
+public class Exercise2 {
+    public static void main (String [] args)
+    {
+        String town;
+        int population;
+        int unemploymentPercent;
+        System.out.printf("City name: %s, Population: %d, Unemployment Percent: %d", town="Mexico City",
+                population=9200000, unemploymentPercent=4);
+        char section;
+        int number;
+        int price;
+        System.out.printf("\nStadium section: %s, Place number: %d, Ticket price: %d", section=67,
+                number=33, price=15);
+        String name;
+        int age;
+        int growth;
+        System.out.printf("\nName: %s, Age: %d, Growth: %d", name="Piter",
+                age=39, growth=170);
+        char symbol;
+        int carNumber;
+        int weight;
+        System.out.printf("\nCarNumber: %s%d%d%d%s%s, Weight: %d", symbol=65,
+                carNumber=0, carNumber=0, carNumber=0, symbol=65, symbol=65, weight=1700);
+        String companyName;
+        int income;
+        int marketRate;
+        System.out.printf("\nCompany name: %s, Year income: %d, Market Rate: %d", companyName="South Deer",
+                income=19200000, marketRate=14);
+
+        System.out.printf("\nRhesus: %s, Blood number: %d, Such people rate: %d", section=45,
+                number=3, price=2);
+        System.out.printf("\nUniversity name: %s, Students number: %d, Deducted Rate: %d", companyName="Ryazan Technical Institute",
+                income=4000, marketRate=22);
+
+        System.out.printf("\nMovie rating: %s, Sold Copies: %d, Rating: %d", section=69,
+                number=3000000, price=100);
+        System.out.printf("\nBrand: %s, Produced: %d, Sold Rate: %d", companyName="Gold South Deer",
+                income=200000, marketRate=76);
+        long barcode;
+        System.out.printf("\nGoods category: %s, Barcode: %d,  Customers older 30 rate: %d", section=69,
+                barcode=3180000120000000l, price=32);
+    }
+}
